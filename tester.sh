6d7b4d80009538dcc0bd3010873b35a792365d03
sof202 <s.o.fletcher@exeter.ac.uk>
2026-03-13 10:35:14 +0000

    fix: capture groups stderr/stdout

--- tester.sh
+++ tester.sh
@@ -7,26 +7,30 @@
 #SBATCH --ntasks-per-node=16
 #SBATCH --mem=1G
 #SBATCH --mail-type=END 
-#SBATCH --output=/dev/null
-#SBATCH --error=/dev/null
+#SBATCH --output=permissions%j.log
+#SBATCH --error=permissions%j.err
 #SBATCH --job-name=permissions
 
 FAILED_LOG="failed.log"
 SUCCESS_LOG="success.log"
 
-# Number of groups should be 102 for me, but maybe there's a chance one or
-# two slip through, I don't fuckin know.
-number_of_groups=$(groups | wc -w)
-if [[ $? -eq 0 ]]; then
+temp_file="/tmp/groups_${SLURM_JOB_ID}_err.tmp"
+groups_output=$(groups 2>"${temp_file}")
+groups_exit=$?
+groups_stderr=$(cat "${temp_file}"; rm "${temp_file}")
+
+if [[ $groups_exit -eq 0 ]]; then
+    number_of_groups=$(echo "${groups_output}" | wc -w)
     if [[ "${number_of_groups}" -le 10 ]]; then
         output_file="${FAILED_LOG}"
         reason="num_groups: ${number_of_groups}"
     else
         output_file="${SUCCESS_LOG}"
+        reason=""
     fi
 else
     output_file="${FAILED_LOG}"
-    reason="${number_of_groups}"
+    reason="${groups_stderr}"
 fi
 
 # I'd love to use a heredoc for this bit, but I don't want to run into problems
